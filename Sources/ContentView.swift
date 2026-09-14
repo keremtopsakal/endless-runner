dc9c3ed61dae1ef2c883e2e435d7c9aa48d21f7b
keremtopsakal <keremtopsakal@hotmail.com>
2026-09-14 08:28:41 +0300

    Initial endless runner game

---
+++ Sources/ContentView.swift
@@ -0,0 +1,24 @@
+﻿import SwiftUI
+import SpriteKit
+
+struct ContentView: View {
+    @State private var scene: GameScene?
+
+    var body: some View {
+        GeometryReader { proxy in
+            SpriteView(scene: currentScene(size: proxy.size))
+                .ignoresSafeArea()
+        }
+    }
+
+    private func currentScene(size: CGSize) -> SKScene {
+        if let scene {
+            return scene
+        }
+        let newScene = GameScene()
+        newScene.size = size
+        newScene.scaleMode = .resizeFill
+        DispatchQueue.main.async { scene = newScene }
+        return newScene
+    }
+}
